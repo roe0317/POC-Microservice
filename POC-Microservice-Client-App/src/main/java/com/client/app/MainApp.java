695d89ab8e7c00e2863bd1223f5097e69b94ad25
roe0317 <100457989+roe0317@users.noreply.github.com>
2022-02-26 18:58:24 +0800

    Update MainApp.java

--- POC-Microservice-Client-App/src/main/java/com/client/app/MainApp.java
+++ POC-Microservice-Client-App/src/main/java/com/client/app/MainApp.java
@@ -1,21 +1,18 @@
 package com.client.app;
 
-
 import java.util.List;
 import java.util.Scanner;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
 
 import com.product.dto.ProductResponseDTO;
+import com.product.dto.RequestDTO;
 import com.product.dto.ResponseDTO;
 
 
 public class MainApp {
-//	private static final String GET_PRODUCT_ENDPOINT_URL = "http://localhost:8080/api/product";
-//	private static final String GET_PRODUCTS_ENDPOINT_URL = "http://localhost:8080/api/product/{productId}";
-//	private static final String CREATE_PRODUCT_ENDPOINT_URL = "http://localhost:8080/ecz/api/products/add";
-//	private static final String UPDATE_PRODUCT_ENDPOINT_URL = "localhost:8080/ecz/api/product/{productId}";
-//	private static final String DELETE_PRODUCT_ENDPOINT_URL = "localhost:8080/ecz/api//product/{productId}";
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
@@ -37,7 +34,8 @@ public class MainApp {
 			main(args);
 			break;
 		case 2:
-			
+			adding();
+			main(args);
 			break;
 		case 3:
 			
@@ -51,20 +49,71 @@ public class MainApp {
 	
 	
 	public static void getProductByAll() {
-
+		
+		
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(org.springframework.http.MediaType.APPLICATION_JSON);
+		
 		RestTemplate restTemplate = new RestTemplate();
 		final String restApi = "http://localhost:8080/ecz/api/products";
 		ProductResponseDTO service = restTemplate.getForObject(restApi, ProductResponseDTO.class);
 		List<ResponseDTO> products = service.getList();
 		for (ResponseDTO displayProduct : products) {	
-//			System.out.println("Product ID       : " + displayProduct.getProductId());
-			System.out.println("Product Brand    : " + displayProduct.getProductBrand());
+
+			System.out.println("\nProduct Brand    : " + displayProduct.getProductBrand());
 			System.out.println("Product Name     : " + displayProduct.getProductName());
 			System.out.println("Product Prize    : " + displayProduct.getProductPrice());
-			System.out.println("Product Quantity : " + displayProduct.getProductQty());
+			System.out.println("Product Quantity : " + displayProduct.getProductQty() + "\n");
 		}
 	}
 	
-	
-	
+	public static void adding() {
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		final String restApi = "http://localhost:8080/ecz/api/products/add";
+		
+		System.out.print("\nENTER PROUCT BRAND  : ");
+		String brand = new Scanner(System.in).next(); 
+		
+		System.out.print("ENTER PROUCT NAME     : ");
+		String name = new Scanner(System.in).next(); 
+		
+		System.out.print("ENTER PROUCT PRIZE    : ");
+		float prize = new Scanner(System.in).nextFloat(); 
+		
+		System.out.print("ENTER PROUCT QUANTITY : ");
+		int qty = new Scanner(System.in).nextInt(); 
+
+		RequestDTO request = new RequestDTO();
+		request.setProductBrand(brand);
+		request.setProductName(name);
+		request.setProductPrice(prize);
+		request.setProductQty(qty);
+//		HttpEntity<RequestDTO> httpEntity = new HttpEntity<>(request, headers);
+		System.out.println("Product Brand   : " + request.getProductBrand());
+		System.out.println("Product Name    : " + request.getProductName());
+		System.out.println("Product Price   : " + request.getProductPrice());
+		System.out.println("Product Quanity : " + request.getProductQty());
+		
+		System.out.println("Would you like to save on file? [Y] || [N]");
+		String optionKey = new Scanner(System.in).next();
+		
+		switch (optionKey.toLowerCase()) {
+		case "y":
+			RestTemplate restTemplate = new RestTemplate();
+			restTemplate.postForObject(restApi, request, ProductResponseDTO.class);
+			break;
+			
+		case "n":
+			main(null);
+			break;
+			
+		default:
+			break;
+		}
+		
+			
+	}
 }
